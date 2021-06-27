19905b12094a650421924ee1371ab53cc3ae44bc
ldr <sholenlee@gmail.com>
2021-06-27 19:31:01 +0800

    p039微调

--- problems/p039_combinationSum.cpp
+++ problems/p039_combinationSum.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class Solution
 {
 private:
-    vector<int> item;
+    vector<int> path;
     vector<vector<int>> ans;
     int sum = 0;
 
@@ -20,53 +20,75 @@ private:
     {
         for (int i = 0; i < candidates.size(); i++)
         {
-            if (item.size() > 0 && item.back() > candidates[i])
+            if (path.size() > 0 && path.back() > candidates[i])
             {
                 continue;
             }
 
             int num = candidates[i];
-            item.push_back(num);
-            auto sum = accumulate(item.begin(),
-                                  item.end(),
+            path.push_back(num);
+            auto sum = accumulate(path.begin(),
+                                  path.end(),
                                   0,
                                   [](auto x, auto y)
                                   { return x + y; });
             if (sum == target)
             {
-                ans.push_back(item);
+                ans.push_back(path);
             }
             else if (sum < target)
             {
                 backtracking_0(candidates, target);
             }
-            item.pop_back();
+            path.pop_back();
         }
     }
 
-    void backtracking(vector<int> &candidates, int target)
+    void backtracking_1(vector<int> &candidates, int target)
     {
         for (int i = 0; i < candidates.size(); i++)
         {
-            if (item.size() > 0 && item.back() > candidates[i])
+            if (path.size() > 0 && path.back() > candidates[i])
             {
                 continue;
             }
 
             int num = candidates[i];
-            item.push_back(num);
+            path.push_back(num);
             sum += num;
 
             if (sum == target)
             {
-                ans.push_back(item);
+                ans.push_back(path);
             }
             else if (sum < target)
             {
-                backtracking(candidates, target);
+                backtracking_1(candidates, target);
             }
             sum -= num;
-            item.pop_back();
+            path.pop_back();
+        }
+    }
+
+    void backtracking_2(vector<int> &candidates, int target, int startIndex)
+    {
+        for (int i = startIndex; i < candidates.size(); i++)
+        {
+            int num = candidates[i];
+            sum += num;
+            path.push_back(num);
+
+            if (sum == target)
+            {
+                ans.push_back(path);
+            }
+            else if (sum < target)
+            {
+                backtracking_2(candidates, target, i);
+            }
+
+            sum -= num;
+            path.pop_back();
         }
     }
 
@@ -74,13 +96,14 @@ public:
     vector<vector<int>> combinationSum(vector<int> &candidates, int target)
     {
         ans.clear();
-        item.clear();
+        path.clear();
         sum = 0;
 
         sort(candidates.begin(), candidates.end());
 
         // backtracking_0(candidates, target);
-        backtracking(candidates, target);
+        // backtracking_1(candidates, target);
+        backtracking_2(candidates, target, 0);
 
         return ans;
     }
