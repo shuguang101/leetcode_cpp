353425d9b4a3d0f1f1e14eceb6000b610afc269e
ldr <sholenlee@gmail.com>
2021-07-27 21:18:44 +0800

    ...

--- cpp20/icu_unicode_char.cpp
+++ cpp20/icu_unicode_char.cpp
@@ -4,14 +4,14 @@
 #include <unicode/ustream.h>
 
 int main(int, char**) {
-  icu_69::UnicodeString us("H世界你好");
+  // icu_69::UnicodeString us("H世界你好");
 
-  std::cout << us[0] << std::endl;
-  std::cout << us[1] << std::endl;
-  std::cout << us.char32At(0) << std::endl;
-  std::cout << us.char32At(1) << std::endl;
+  // std::cout << us[0] << std::endl;
+  // std::cout << us[1] << std::endl;
+  // std::cout << us.char32At(0) << std::endl;
+  // std::cout << us.char32At(1) << std::endl;
 
-  std::cout << us.indexOf("你") << std::endl;
+  // std::cout << us.indexOf("你") << std::endl;
 
-  std::cout << us << std::endl;
+  // std::cout << us << std::endl;
 }
