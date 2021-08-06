5318641dd853639b774405c429a65d9ed9091d13
ldr <sholenlee@gmail.com>
2021-08-06 17:33:01 +0800

    p452 区间重叠问题 用最少数量的箭引爆气球

---
+++ problems/p452_findMinArrowShots.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int findMinArrowShots(vector<vector<int>>& points) {
+    sort(points.begin(), points.end());
+    int left = points[0][0];
+    int right = points[0][1];
+
+    int len = points.size();
+    int ans = len;
+    for (int i = 1; i < len; i++) {
+      if (points[i][0] <= right) {
+        left = points[i][0];
+        right = min(right, points[i][1]);
+        ans--;
+      } else {
+        left = points[i][0];
+        right = points[i][1];
+      }
+    }
+
+    return ans;
+  };
+
+  int main(int, char**) {}
